03258f53b6fd647a4a2c5663ccac9d914622585c
Muhammad Irfan <irfan.abrazak@gmail.com>
2019-09-21 16:41:09 +0800

    Fetch more data at bottom page.

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 class Home extends React.Component {
+  page = 2;
   constructor(props) {
     super(props);
     this.state = {
@@ -15,7 +16,25 @@ class Home extends React.Component {
       .then(res => res.json())
       .then(data => {
         this.setState({ lists: data.data })
+        // console.log(this.state.lists)
       });
+
+    window.addEventListener('scroll', (event) => {
+
+      if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        console.log("you're at the bottom of the page");
+        fetch('https://cdn-discover.hooq.tv/v1.2/discover/feed?region=ID&page=' + this.page + '&perPage=20')
+          .then(res => res.json())
+          .then(data => {
+            // console.log(data.data)
+            // this.state.lists.push(data.data);
+            this.setState({ lists: [...this.state.lists, ...data.data] })
+          });
+        this.page++;
+      }
+
+    });
+
 
   }
 
@@ -29,7 +48,7 @@ class Home extends React.Component {
           {
             lists.map(list => {
               if (list.type === "Multi-Title-Manual-Curation") {
-                console.log(list);
+                // console.log(list);
                 return (
                   <div key={list.row_id}>
                     <p>{list.row_name}</p>

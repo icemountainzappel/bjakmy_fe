8bee69fb8508668bfe574e0881e4cea6c62507d3
Muhammad Irfan <irfan.abrazak@gmail.com>
2019-09-21 15:38:03 +0800

    Fetch API and filter data type

--- src/Detail.js
+++ src/Detail.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
-const Detail = () => (
-  <div>
-    Detail
-  </div>
-)
+class Detail extends React.Component {
+  state = { counter: 1 };
+
+  handleClick = () => {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  };
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.counter}
+      </button>
+    );
+  }
+}
 
 export default Detail
--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,46 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Home = () => (
-  <div>
-    <button> <Link to='/Detail'>BJAKMY</Link></button>
-  </div>
-)
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lists: []
+    };
+  }
+
+  componentDidMount() {
+
+    fetch('https://cdn-discover.hooq.tv/v1.2/discover/feed?region=ID&page=1&perPage=20')
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ lists: data.data })
+      });
+
+  }
+
+  render() {
+    const { lists } = this.state;
+
+    return (
+      <div>
+        <p>Hello</p>
+        <ul>
+          {
+            lists.map(list => {
+              if (list.type === "Multi-Title-Manual-Curation") {
+                return (
+                  <li key={list.row_id}>
+                    <a href={list.row_id}>{list.type}</a>
+                  </li>
+                )
+              }
+            })
+          }
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default Home

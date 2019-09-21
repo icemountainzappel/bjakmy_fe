c7caa739a7732627e1c4239607178876695a642b
icemountainzappel <secondwave89@yahoo.com>
2019-09-21 09:40:00 +0800

    Added router

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Home = () => (
+  <div>
+    <button> <Link to='/Detail'>BJAKMY</Link></button>
+  </div>
+)
+
+export default Home
